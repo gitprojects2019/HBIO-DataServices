a5b7452c8d9eaade9ad6df05dd75a8cc05588748
GitHub Checkins Only <git_projects@hotmail.com>
2019-02-12 09:44:28 +0530

    player model

--- Services/Models/PlayerDetails.cs
+++ Services/Models/PlayerDetails.cs
@@ -2,23 +2,55 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Services.Models
 {
     public class PlayerDetails
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
+
+        [BsonElement("name")]
         public string Name { get; set; }
+
+        [BsonElement("registrationId")]
         public string RegistrationId { get; set; }
+
+        [BsonElement("governmentId")]
         public string GovernmentId { get; set; }
+
+
+        [BsonElement("governmentIdType")]
         public string GovernmentIdType { get; set; }
+
+        [BsonElement("dob")]
+        public DateTime DateOfBirth { get; set; }
+
+        [BsonElement("age")]
         public string Age { get; set; }
+
+        [BsonElement("weight")]
         public string Weight { get; set; }
+
+        [BsonElement("contactNumber")]
         public string ContactNumber { get; set; }
+
+        [BsonElement("Name")]
         public string State { get; set; }
+
+        [BsonElement("Name")]
         public string District { get; set; }
+
+        [BsonElement("Name")]
         public string Gender { get; set; }
+
+        [BsonElement("Name")]
         public string TournamentName { get; set; }
-        public string TournamentDate { get; set; }
+
+        [BsonElement("Name")]
+        public DateTime TournamentDate { get; set; }
     }
 }
